c14c4eb5c8c7e5cf9548b1b30bfe1ba0d7db37d9
sashbhagav <sashreek_bhagav@hotmail.com>
2019-03-23 16:27:58 -0500

    second

--- speech.py
+++ speech.py
@@ -2,21 +2,12 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-# with sr.WaveFile("test.wav") as source:
-#     audio = r.record(source)
-
-#     try:
-#         print("Transcribtion: {}".format(r.recognize(audio)))
-#     except:
-#         print("Could not recognize audio")
-
-
-#If we want to directly hook the microphone up to this app then
-
 with sr.Microphone() as source:
-    audio = r.record(source)
-
+    print("speak now : ")
+    audio = r.record(source, duration = 10)
+    
     try:
-        print("Transcribtion: {}".format(r.recognize(audio)))
+        text = r.recognize_google(audio)
+        print("you said :{} ".format(text))
     except:
-        print("Could not recognize audio")
+        print("didnt recognize")
